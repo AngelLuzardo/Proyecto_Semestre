d0f3af8ae18495a6f46fec518aad2beaf30402d5
AngelLuzardo <105563411+AngelLuzardo@users.noreply.github.com>
2022-05-14 15:08:03 -0400

    Add files via upload

---
+++ Proyecto_Semestre/vista/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../public/default.css">
+    <link rel="stylesheet" href="../public/normalize.css">
+    <link rel="stylesheet" href="../public/index.css">
+    <script src="https://kit.fontawesome.com/3879fbe72a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="header">
+        <div class="header__contenedor">
+            <div class="header__logo">
+                            
+            </div>
+            <div class="header__search">
+                <div class="search">
+                    <input type="text" name="barra_busqueda" class="search__input" placeholder="Buscar productos...">
+                    <button class="search__boton">
+                        <i class="search__icon fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div>
+                
+            </div>
+            <div class="header__usuario-carro">
+                <div class="header__usuario">
+                    <i class="fa-solid fa-user"></i>
+                </div>
+                <p>Mi cuenta</p>
+                <div class="header__carro">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>   
+                <p>Mi carro</p>
+            </div>
+        </div>
+    </div>
+    <div class="nav">
+        <div class="nav__contenedor">
+            <ul>
+                <li><a href="#">Componentes</a></li>
+                <li><a href="#">Suministro Oficina</a></li>
+                <li><a href="#">Audio</a></li>
+                <li><a href="#">Conectividad y Redes</a></li>
+                <li><a href="#">Gaming</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="slider"></div>
+    
+
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus nostrum rem error, praesentium, accusantium odit, maxime accusamus laudantium minus nobis voluptas amet tempore! Eveniet, atque nemo delectus tempore praesentium numquam?
+
+    </h1>
+
+
+</body>
+</html>
